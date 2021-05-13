24b0536b3de3678d2dec4eacd8f4637c5b3da009
haschish <haschish@gmail.com>
2021-05-13 16:47:28 +0500

    added boilerplate for SurfaceView

---
+++ app/src/main/java/com/example/gleb/Game.java
@@ -0,0 +1,80 @@
+package com.example.gleb;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class Game extends SurfaceView implements Runnable {
+    private final int MILLIS_IN_SECOND = 1000;
+    private Thread thread;
+    private SurfaceHolder holder;
+    private long fps;
+    private volatile boolean isPlaying;
+    private boolean isPaused = false;
+
+    public Game(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        holder = getHolder();
+    }
+
+    private void draw() {
+        if (holder.getSurface().isValid()) {
+            Canvas canvas = holder.lockCanvas();
+
+            canvas.drawColor(Color.BLUE);
+
+            printDebuggingText(canvas);
+
+            holder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    private void printDebuggingText(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setTextSize(60);
+        canvas.drawText("FPS: " + fps, 30, 90, paint);
+    }
+
+    private void update() {
+    }
+
+    public void pause() {
+        isPlaying = false;
+        try {
+            thread.join();
+        } catch(Exception e) {
+            Log.e("Error", "joining thread");
+        }
+    }
+
+    public void resume() {
+        isPlaying = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        while(isPlaying) {
+            long frameStartTime = System.currentTimeMillis();
+
+            if (!isPaused) {
+                update();
+            }
+            draw();
+
+            long timeThisFrame = System.currentTimeMillis() - frameStartTime;
+            if (timeThisFrame > 0) {
+                fps = MILLIS_IN_SECOND / timeThisFrame;
+            }
+        }
+
+        draw();
+    }
+}
--- app/src/main/java/com/example/gleb/MainActivity.java
+++ app/src/main/java/com/example/gleb/MainActivity.java
@@ -5,10 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        game = findViewById(R.id.gameView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        game.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        game.resume();
     }
 }
