cd48bc5c16079c89bc11b0cc47d3d853e55fdcce
haschish <haschish@gmail.com>
2021-05-20 18:16:23 +0500

    added Control

---
+++ app/src/main/java/com/example/gleb/Control.java
@@ -0,0 +1,91 @@
+package com.example.gleb;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class Control extends View {
+    private int width;
+    private int height;
+    private boolean isPress = false;
+    private float startX;
+    private float startY;
+    private float shiftX = 0;
+    private float shiftY = 0;
+
+    OnChangeSpeed changeSpeedListener;
+
+    public Control(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        width = canvas.getWidth();
+        height = canvas.getHeight();
+        float centerX = width / 2;
+        float centerY = width / 2;
+        float radiusOut = width / 2;
+        float radiusIn = width / 4;
+
+        Paint paint = new Paint();
+        paint.setColor(Color.argb(120, 255, 255, 255));
+        canvas.drawCircle(centerX, centerY, radiusOut, paint);
+
+        paint.setColor(Color.WHITE);
+        if (isPress) {
+            float cx = centerX + shiftX;
+            float cy = centerY + shiftY;
+            canvas.drawCircle(cx, cy, radiusIn, paint);
+        } else {
+            canvas.drawCircle(centerX, centerY, radiusIn, paint);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN && event.getPointerCount() == 1) {
+            isPress = true;
+            startX = event.getX();
+            startY = event.getY();
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            shiftX = event.getX() - startX;
+            shiftY = event.getY() - startY;
+
+            if (changeSpeedListener != null) {
+                changeSpeedListener.onChange(shiftX, shiftY);
+            }
+            invalidate();
+        } else if (event.getAction() == MotionEvent.ACTION_UP && event.getPointerCount() == 1) {
+            isPress = false;
+            resetShift();
+            invalidate();
+        }
+        return true;
+    }
+
+    private void resetShift() {
+        shiftX = 0;
+        shiftY = 0;
+        if (changeSpeedListener != null) {
+            changeSpeedListener.onChange(shiftX, shiftY);
+        }
+    }
+
+    void setOnChangeListener(OnChangeSpeed listener) {
+        changeSpeedListener = listener;
+    }
+
+    public static interface OnChangeSpeed {
+        void onChange(float vx, float vy);
+    }
+}
--- app/src/main/java/com/example/gleb/MainActivity.java
+++ app/src/main/java/com/example/gleb/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     Game game;
+    Control control;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         game = findViewById(R.id.gameView);
+        control = findViewById(R.id.control);
+        control.setOnChangeListener((float vx, float vy) -> {
+            game.setHeroSpeed(vx, vy);
+        });
     }
 
     @Override
